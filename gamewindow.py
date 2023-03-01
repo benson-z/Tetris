134681942dae211ac37b46a9e9a2123c518c67cc
benson-z <bensonzhou@yahoo.com>
2023-03-01 14:15:16 -0800

    fix crashing on 180 kick

--- gamewindow.py
+++ gamewindow.py
@@ -5,6 +5,8 @@ gameDisplay = pygame.display.set_mode(resolution)
 pygame.display.set_caption("Tetris")
 clock = pygame.time.Clock()
 
+# pygame.key.set_repeat(100, 1)
+
 def getInstance():
     return gameDisplay
 
